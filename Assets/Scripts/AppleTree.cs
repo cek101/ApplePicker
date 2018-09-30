0046e5f14d5ca2cd4d9d6fed42f6c78764b27bef
cek101 <43051991+cek101@users.noreply.github.com>
2018-09-29 23:56:39 -0400

    1st commit
    stuck on catching Apples in book p. 483.
    CollidedWith code complier error.

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    [Header ("Set in Inspector")]
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which the appleTree moves
+    public float    speed = 1f;
+
+    //distance where appleTree turns around
+    public float    leftAndRightEdge = 10f;
+
+    //chance that AppleTree will change directions
+    public float    chanceToChangeDirections = 0.1f;
+
+    //rate at which Apples will be instatiated
+    public float    secondsBetweenAppleDrops = 1f;
+
+         
+	void Start () {
+        //dropping apples every second
+        Invoke("DropApple", 2f); // a 
+		
+	}
+
+    void DropApple ()   { // b 
+        GameObject apple = Instantiate<GameObject>(applePrefab); // c
+        apple.transform.position = transform.position; // d
+        Invoke("DropApple", secondsBetweenAppleDrops); // e
+    }
+
+    // Update is called once per frame
+    void Update()   { //f
+        //basic Movement
+        Vector3 pos = transform.position; //b
+        pos.x += speed * Time.deltaTime; //c 
+        transform.position = pos;  //d
+
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge)  {
+            speed = Mathf.Abs(speed); //move right
+
+        }
+        else if (pos.x > leftAndRightEdge)  {
+            speed = -Mathf.Abs(speed); //move left
+        } //else if ( Random.value < chanceToChangeDirections )   { //a
+    }    //    speed *= -1; // change direction //b
+
+    void FixedUpdate () {  
+        if (Random.value < chanceToChangeDirections)    { //a
+            speed *= -1; // change direction //b
+        }
+    }
+}
+
---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour     {
+
+    // Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    void Update()    {
+        //Get the current screen position of the mouse from input
+        Vector3 mousePos2D = Input.mousePosition; //a
+
+        // The camera's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z; //b 
+
+        //convert the point from 2d screen space into 3d game world space
+        Vector3 mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2D); //c
+
+        // move the x position of this Basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3d.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)       { //a catching apples page 483 check code in book?
+      //find out what hit this basket
+        GameObject collideWith = coll.gameObject; //b
+        if (collidedWith.tag == "Apple")  {  //c
+            Destroy(collidedWith);
+
+        }
+
+    }
+}
+
